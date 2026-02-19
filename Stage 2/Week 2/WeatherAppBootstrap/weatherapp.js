0b4a91fef8f38504fc41873cae162c079cfa422d
Valerie <valerie.pietroluongo@gmail.com>
2026-02-18 23:40:46 -0600

    WeatherAppBootstrap svg rect working and comment clean-up

--- Stage 2/Week 2/WeatherAppBootstrap/weatherapp.js
+++ Stage 2/Week 2/WeatherAppBootstrap/weatherapp.js
@@ -15,103 +15,20 @@ async function getForecast(){
 
         for (let day in jsonData.properties.periods){
             console.log(jsonData);
-            //document.getElementById("forecast").innerHTML += `<p>${jsonData.properties.periods[day].name}: ${jsonData.properties.periods[day].temperature}${jsonData.properties.periods[day].temperatureUnit} <img src=\"${jsonData.properties.periods[day].icon}\"></p>`;
             const forecastInfo= {
                                 day: jsonData.properties.periods[day].name, 
-                                temperatureString: jsonData.properties.periods[day].temperature + jsonData.properties.periods[day].temperatureUnit,
+                                temperatureString: jsonData.properties.periods[day].temperature + " " + jsonData.properties.periods[day].temperatureUnit,
                                 shortForecast: jsonData.properties.periods[day].shortForecast,
                                 weatherIconURL: jsonData.properties.periods[day].icon 
                                 };
             document.getElementById("forecastDay").innerHTML += `<p>${forecastInfo.day}</p>`;
             document.getElementById("forecastTemperature").innerHTML += `<p>${forecastInfo.temperatureString}</p>`;
             document.getElementById("forecastDescription").innerHTML += `<p>${forecastInfo.shortForecast}</p>`;
-            // document.getElementById("forecastDay").innerHTML += `<p>${jsonData.properties.periods[day].name}</p>`;
-            // document.getElementById("forecastTemperature").innerHTML += `<p>${jsonData.properties.periods[day].temperature} ${jsonData.properties.periods[day].temperatureUnit}</p>`;
-            // document.getElementById("forecastDescription").innerHTML += `<p>${jsonData.properties.periods[day].shortForecast}</p>`;
-            //document.getElementById("weatherIcon").src += jsonData.properties.periods[day].icon; <img src=\"jsonData.properties.periods[day].icon\">
+            
             forecastInfoArray.push(forecastInfo);
         }
-        //populateCarousel(forecastInfoArray);
-        const carouselInner = document.querySelector('#myCarousel .carousel-inner');
-        const carouselIndicators = document.querySelector('#myCarousel .carousel-indicators');
-        
-        carouselInner.innerHTML = '';
-        carouselIndicators.innerHTML = '';
-        
-        for (let i = 0; i <forecastInfoArray.length; i++){
-            
-            // if (i != 1)
-            //     continue;
 
-            // 1. Create the carousel item (slide)
-            const carouselItem = document.createElement('div');
-            carouselItem.classList.add('carousel-item');
-            //carouselItem.style.backgroundcolor = "#777";
-            
-            const svg = document.createElement('svg');
-            //svg.classList.add("bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"');
-            svg.classList.add("bd-placeholder-img");
-            svg.setAttribute("width","100%");
-            svg.setAttribute("height","100%");
-            svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-            svg.setAttribute("aria-hidden", "true");
-            svg.setAttribute("preserveAspectRatio", "xMidYMid slice");
-            svg.setAttribute("focusable","false");
-            //const rect = document.createElement('rect');
-            //rect.classList.add("myCarousel-rect");
-            //rect.setAttribute("width", "100%");
-            //rect.setAttribute("height", "100%");
-            //rect.setAttribute("fill", "#777");
-            //svg.appendChild(rect);
-            svg.innerHTML = '<rect width="100%" height="100%" fill="#777"/>';
-            //let svg = '<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>'
-            //carouselItem.appendChild(svg);
-            carouselItem.appendChild(svg);
-            
-            // The first item must have the 'active' class
-            if (i === 0) {
-                carouselItem.classList.add('active');
-            }
-
-            const carouselContainer = document.createElement("div");
-            carouselContainer.classList.add("container");
-
-            const carouselCaption = document.createElement('div');
-            carouselCaption.classList.add("carousel-caption");
-        
-            // 2. Add the image element inside the item
-            const img = document.createElement("img");
-            img.src = forecastInfoArray[i].weatherIconURL;
-            //img.classList.add('d-block', 'w-100'); // Bootstrap classes for responsive images
-            img.classList.add("myCarousel-image"); 
-            img.alt = `Slide ${i + 1}`;
-            //carouselCaption.append(`<p>${forecastInfoArray[i].day}</p>`, img);
-            carouselCaption.innerHTML = `<h1 style="color:black">${forecastInfoArray[i].day}</h1>
-                                         <p style="color:black">${forecastInfoArray[i].temperatureString}</p>
-                                         <p style="color:black">${forecastInfoArray[i].shortForecast}</p>`;
-            carouselCaption.appendChild(img);
-        
-            carouselContainer.appendChild(carouselCaption);
-            carouselItem.appendChild(carouselContainer);
-            // 3. Append the item to the carousel inner
-            carouselInner.appendChild(carouselItem);
-
-            // 4. Create the indicator button
-            const indicatorButton = document.createElement('button');
-            indicatorButton.type = 'button';
-            indicatorButton.setAttribute('data-bs-target', '#myCarousel');
-            indicatorButton.setAttribute('data-bs-slide-to', i);
-            indicatorButton.setAttribute('aria-label', `Slide ${i + 1}`);
-            // The first indicator must also be 'active'
-            if (i === 0) {
-            indicatorButton.classList.add('active');
-            indicatorButton.setAttribute('aria-current', 'true');
-            }
-
-            // 5. Append the indicator to the carousel indicators container
-            carouselIndicators.appendChild(indicatorButton);
-            
-        }
+        populateCarousel(forecastInfoArray);
     }
     else {
         document.getElementById("forecast").innerHTML = `Error status: ${response.status}`;
@@ -119,4 +36,91 @@ async function getForecast(){
 }
 
 function populateCarousel(forecastInfoArray){
+
+    const carouselInner = document.querySelector('#myCarousel .carousel-inner');
+    const carouselIndicators = document.querySelector('#myCarousel .carousel-indicators');
+    
+    carouselInner.innerHTML = '';
+    carouselIndicators.innerHTML = '';
+    
+    for (let i = 0; i <forecastInfoArray.length; i++){
+    
+        // 1. Create the carousel item (slide)
+        const carouselItem = document.createElement('div');
+        carouselItem.classList.add('carousel-item');
+        
+        // The first item must have the 'active' class
+        if (i === 0) {
+            carouselItem.classList.add('active');
+        }
+        // 2. Create the svg 
+        const svgNS = "http://www.w3.org/2000/svg";
+        const svg = document.createElementNS(svgNS, "svg");
+        svg.classList.add("bd-placeholder-img");
+        svg.setAttribute("width","100%");
+        svg.setAttribute("height","100%");
+        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.setAttribute("aria-hidden", "true");
+        svg.setAttribute("preserveAspectRatio", "xMidYMid slice");
+        svg.setAttribute("focusable","false");
+
+        // 3.  Create the rect (the grey background rectangle)  
+        const rect = document.createElementNS(svgNS, "rect");
+        rect.classList.add("myCarousel-rect");
+        rect.setAttribute("width", "100%");
+        rect.setAttribute("height", "100%");
+        rect.setAttribute("fill", "#777");
+
+        // 4. Add the rectangle element inside the svg
+        svg.appendChild(rect);
+ 
+        // 5.  Add the svg element inside the carousel item
+        carouselItem.appendChild(svg);
+        
+        // 6. Create the carousel container
+        const carouselContainer = document.createElement("div");
+        carouselContainer.classList.add("container");
+    
+        // 7. Create the carousel caption
+        const carouselCaption = document.createElement('div');
+        carouselCaption.classList.add("carousel-caption");
+    
+        // 8. Create the image element for the weather icon
+        const img = document.createElement("img");
+        img.src = forecastInfoArray[i].weatherIconURL;
+         
+        img.classList.add("myCarousel-image"); 
+        img.alt = `Slide ${i + 1}`;
+        
+        // 9. Add the forecast info inside the carousel caption and then the image element
+        carouselCaption.innerHTML = `<h1>${forecastInfoArray[i].day}</h1>
+                                     <p>${forecastInfoArray[i].temperatureString}</p>
+                                     <p>${forecastInfoArray[i].shortForecast}</p>`;
+        carouselCaption.appendChild(img);
+    
+        // 10.  Add the carousel caption to the carousel container
+        carouselContainer.appendChild(carouselCaption);
+
+        // 11.  Append the carousel container to the carousel item  
+        carouselItem.appendChild(carouselContainer);
+
+        // 12. Append the item to the carousel inner
+        carouselInner.appendChild(carouselItem);
+    
+        // 13. Create the indicator button
+        const indicatorButton = document.createElement('button');
+        indicatorButton.type = 'button';
+        indicatorButton.setAttribute('data-bs-target', '#myCarousel');
+        indicatorButton.setAttribute('data-bs-slide-to', i);
+        indicatorButton.setAttribute('aria-label', `Slide ${i + 1}`);
+        // The first indicator must also be 'active'
+        if (i === 0) {
+        indicatorButton.classList.add('active');
+        indicatorButton.setAttribute('aria-current', 'true');
+        }
+    
+        // 14. Append the indicator to the carousel indicators container
+        carouselIndicators.appendChild(indicatorButton);
+        
+    }
 }
