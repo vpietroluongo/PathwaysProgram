5961fd1639a3861dff2210d552bd0fc17bd82183
Valerie <valerie.pietroluongo@gmail.com>
2026-02-20 09:33:10 -0600

    week 7 challenge OpenLibrary code cleanup, comments added, and added authorNameDisplayTop

--- Stage 2/Week 2/Week7Challenge/week7challenge.js
+++ Stage 2/Week 2/Week7Challenge/week7challenge.js
@@ -1,14 +1,5 @@
 async function lookupAuthor(){
-    let apiString = "https://openlibrary.org/";
-    let author = (document.getElementById("author").value);
-    //author = author.replaceAll(". ", "%20");
-    //author = author.replaceAll(" ", "%20");
-    author = encodeURIComponent(author);
-    apiStringAuthor = apiString + "search/authors.json?q=" + author;
-    alert(apiStringAuthor);
-
-    let response = await fetch(apiStringAuthor);
-
+    //clear previous HTML elements
     document.getElementById("coverSpot").innerHTML = "";
     document.getElementById("authorImageSpot").innerHTML = "";
     document.getElementById("authorName").innerHTML = "";
@@ -16,60 +7,75 @@ async function lookupAuthor(){
     document.getElementById("authorInfo").innerHTML = "";
     bookTitle.innerHTML = "";
     document.getElementById("publishDate").innerHTML = "";
+    document.getElementById("authorNameDisplayTop").innerHTML = "";
+
+    //setup first API call to get author key and their top work
+    let apiString = "https://openlibrary.org/";
+    let author = (document.getElementById("author").value);
+    author = encodeURIComponent(author);  //encodes characters, such as spaces as %20
+    apiStringAuthor = apiString + "search/authors.json?q=" + author;
+    console.log(`apiStringAuthor: ${apiStringAuthor}`);
+    
+    let response = await fetch(apiStringAuthor);
+    
 
     let myBookBox = document.getElementById("myBookBox");  
     if (response.status >= 200 && response.status <= 299){
         let jsonData = await response.json();
         
+        //check if author exists
         if (jsonData.numFound != 0){
             myBookBox.classList.remove("my-box-hidden");
             
             let authorKey = jsonData.docs[0].key;
-            console.log(authorKey);
+            console.log(`Author key: ${authorKey}`);
             
             let topWork = jsonData.docs[0].top_work;
-            console.log(topWork);
+            console.log(`Top work: ${topWork}`);
             let topWorkFormatted = encodeURIComponent(topWork);
-            //topWorkFormatted = topWork.replaceAll(" ", "%20");
 
+            //setup second API call using author key to get author info, such as their bio, birth date, and personal name
             let apiStringAuthorInfo = apiString + "authors/" + authorKey + ".json";
-            alert(apiStringAuthorInfo);
+            console.log(`apiStringAuthorInfo: ${apiStringAuthorInfo}`);
             response = await fetch(apiStringAuthorInfo);
             if (response.status >= 200 && response.status <= 299){
                 jsonData = await response.json();
+
+                //check if author has a bio in bio.value, and if so, display it. If not, hide the author info box
                 if (jsonData.bio && jsonData.bio.value){
                     if (document.getElementById("authorInfoBox").classList.contains("my-box-hidden")){
                         document.getElementById("authorInfoBox").classList.remove("my-box-hidden");
-                        document.getElementById("authorInfo").innerHTML = jsonData.bio.value;
-                        document.getElementById("authorInfo").style.fontSize = "10px";
                     }
-                    else {
-                        document.getElementById("authorInfo").innerHTML = jsonData.bio.value;
-                        document.getElementById("authorInfo").style.fontSize = "10px";
-
+                     if (document.getElementById("authorNameDisplayTop").classList.contains("my-box-hidden")) {
+                        document.getElementById("authorNameDisplayTop").classList.remove("my-box-hidden");
                     }
-                    
-                    
+                    document.getElementById("authorInfo").innerHTML = jsonData.bio.value;
+                    document.getElementById("authorInfo").style.fontSize = "10px";
                     document.getElementById("authorName").innerHTML = jsonData.personal_name;
+                    document.getElementById("authorNameDisplayTop").innerHTML = jsonData.personal_name;
                     document.getElementById("birthday").innerHTML = jsonData.birth_date;
-                    authorInfoBoxSetup(authorKey);
+
+                    authorImageBoxSetup(authorKey);
                 }
+                //check if author has a bio in bio, and if so, display it. If not, hide the author info box
                 else if (jsonData.bio && (typeof jsonData.bio === "string")){
+                    //check if author info box is hidden, and if so, show it
                     if (document.getElementById("authorInfoBox").classList.contains("my-box-hidden")) {
                         document.getElementById("authorInfoBox").classList.remove("my-box-hidden");
-                        document.getElementById("authorInfo").innerHTML = jsonData.bio;
-                        document.getElementById("authorInfo").style.fontSize = "10px";
                     }
-                    else {
-                        document.getElementById("authorInfo").innerHTML = jsonData.bio;
-                        document.getElementById("authorInfo").style.fontSize = "10px";
-
+                    if (document.getElementById("authorNameDisplayTop").classList.contains("my-box-hidden")) {
+                        document.getElementById("authorNameDisplayTop").classList.remove("my-box-hidden");
                     }
-
+               
+                    document.getElementById("authorInfo").innerHTML = jsonData.bio;
+                    document.getElementById("authorInfo").style.fontSize = "10px";
                     document.getElementById("authorName").innerHTML = jsonData.personal_name;
+                    document.getElementById("authorNameDisplayTop").innerHTML = jsonData.personal_name;
                     document.getElementById("birthday").innerHTML = jsonData.birth_date;
-                    authorInfoBoxSetup(authorKey);
-                }   
+
+                    authorImageBoxSetup(authorKey);
+                } 
+                //if author doesn't have a bio, hide the author info box
                 else{
                     document.getElementById("authorInfoBox").classList.add("my-box-hidden");
                 }
@@ -79,52 +85,27 @@ async function lookupAuthor(){
                 alert(`Error: ${response.status}`);
             }
             
+            //setup third API call using author key and top work to get book info, such as the cover key and publish date
             let apiStringTopWork = apiString + "search.json?q=" + topWorkFormatted + "&author=" + authorKey;
+            console.log(`apiStringTopWork: ${apiStringTopWork}`);
             response = await fetch(apiStringTopWork);
             jsonData = await response.json();
+
             let coverKey = jsonData.docs[0].cover_edition_key;
-            console.log(coverKey);
+            console.log(`coverKey: ${coverKey}`);
             let publishDate = jsonData.docs[0].first_publish_year;
             document.getElementById("publishDate").innerHTML = publishDate;
             let bookTitle = document.getElementById("bookTitle");
             bookTitle.innerHTML = topWork;
-
             
-            
+            //setup URL to get the book cover
             let coverURL = `https://covers.openlibrary.org/b/olid/${coverKey}-M.jpg`;
-            console.log(coverURL);
+            console.log(`Cover URL: ${coverURL}`);
 
             const myDiv = document.querySelector('#myDiv .row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative');
             const img = document.createElement("img");
-            // if (coverKey == undefined){
-            //     const svgNS = "http://www.w3.org/2000/svg";
-            //     const svg = document.createElementNS(svgNS, "svg");
-            //     svg.classList.add("bd-placeholder-img");
-            //     svg.setAttribute("width","200");
-            //     svg.setAttribute("height","250");
-            //     svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-            //     svg.setAttribute("aria-hidden", "true");
-            //     svg.setAttribute("preserveAspectRatio", "xMidYMid slice");
-            //     svg.setAttribute("focusable","false");
-
-            //     // 3.  Create the rect (the grey background rectangle)  
-            //     const rect = document.createElementNS(svgNS, "rect");
-            //     rect.setAttribute("width", "100%");
-            //     rect.setAttribute("height", "100%");
-            //     rect.setAttribute("fill", "#55595c");
-                
-            //     const text = document.createElementNS(svgNS, "text");
-            //     text.setAttribute("x", "50%");
-            //     text.setAttribute("y", "50%");
-            //     text.setAttribute("fill", "#eceeef");
-            //     text.setAttribute("dy", ".3em");
-             
-                
-            //     text.innerHTML = "Cover Not Found";
-            //     svg.appendChild(rect);
-            //     svg.appendChild(text);
-            //     document.getElementById("coverSpot").appendChild(svg);
-            // }
+  
+            //check if cover key exists, and if so, display the cover. If not, display an "image not found" placeholder
             if (coverKey == undefined) {
                 imageNotFoundBoxSetup("cover");
             }
@@ -138,19 +119,21 @@ async function lookupAuthor(){
             }
         }
         else{
-             myBookBox.classList.add("my-box-hidden");
+            //hide book info and author info boxes if author isn't found, and alert the user 
+            myBookBox.classList.add("my-box-hidden");
             alert(`Author ${author} not found.`);
+            document.getElementById("authorNameDisplayTop").innerHTML = "Author Not Found";
         }
     }
     else{
         alert(`Error: ${response.status}`);
     }
 
-
+    //clear the input text box after search
     document.getElementById("author").value = "";
 }
 
-
+//set up an "image not found" placeholder using svg if the book cover or author image isn't found
 function imageNotFoundBoxSetup(flag){
         const svgNS = "http://www.w3.org/2000/svg";
         const svg = document.createElementNS(svgNS, "svg");
@@ -187,16 +170,16 @@ function imageNotFoundBoxSetup(flag){
         }
 }
 
-
-function authorInfoBoxSetup(authorKey, authorName){      
+//set up the box containing the author image
+function authorImageBoxSetup(authorKey, authorName){      
     let authorURL = `https://covers.openlibrary.org/a/olid/${authorKey}-M.jpg`;
 
     const myDiv = document.querySelector('#myDiv .row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative');
     const img = document.createElement("img");
 
+    //check if author key exists, and if so, display the author image. If not, display an "image not found" placeholder
     if (authorKey == undefined) {
         imageNotFoundBoxSetup("author");
-
     }
     else {
         img.src = authorURL;
