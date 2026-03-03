5d53d4902b4e6c1a1fbe9dbf808f01fcfe08e7df
Valerie <valerie.pietroluongo@gmail.com>
2026-03-03 11:23:17 -0600

    TodoApi: Tutorial- Create a controller-based web API with ASP.NET Core .NET version 8

---
+++ Stage 2/Week 4/TodoApi/TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace TodoApi.Models;
+public class TodoItemDTO
+{
+    public long Id { get; set; }
+    public string? Name { get; set;  }
+    public bool IsComplete { get; set; }
+}
+
