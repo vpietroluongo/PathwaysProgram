707b9ea25ca95d25d389610d2ff9b871114c38e8
Valerie <valerie.pietroluongo@gmail.com>
2026-05-22 16:42:14 -0500

    Stage 5 Week 3 TodoMvcApp working after adding create html

--- Stage 4/Week_3/TodoMvcApp/Controllers/TodoController.cs
+++ Stage 4/Week_3/TodoMvcApp/Controllers/TodoController.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel.Design;
+using Microsoft.AspNetCore.Mvc;
 
 public class TodoController : Controller
 {
--- Stage 4/Week_3/TodoMvcApp/Program.cs
+++ Stage 4/Week_3/TodoMvcApp/Program.cs
@@ -9,6 +9,6 @@ app.UseStaticFiles();
 app.UseRouting();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Todo}/{action=Index}/{id?}");
 
 app.Run();
